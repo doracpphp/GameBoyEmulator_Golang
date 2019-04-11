ef98387fec0a05a140f6f29f8717c6ffdb077ec1
doracpphp <dora.cpphp@gmail.com>
2019-04-11 11:41:33 +0900

    change

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import "github.com/doracpphp/GameBoyEmulator_Golang/instruction"
 
-
-func main(){
+func main() {
 	cpu := instruction.NewEmulator()
 	cpu.Registers.SetCarry(1)
-	cpu.Registers.SP=0xFFFE
-	cpu.Registers.B=0x2
-	cpu.Registers.A=0x1
+	cpu.Registers.SP = 0xFFFE
+	cpu.Registers.B = 0x2
+	cpu.Registers.A = 0x1
 	cpu.Debug()
-	instruction.ADDr_b(cpu);
+	instruction.ADDr_b(cpu)
 	cpu.Debug()
 	cpu.Inst[0x00](cpu)
 
